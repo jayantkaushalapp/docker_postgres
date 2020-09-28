70c59053cef2eadffad87b9e5534d2e4e3c22374
Jayant Kaushal <awakenrogue@gmail.com>
2020-09-29 02:00:42 +0530

    updating script to make sure dump is imported in superset db.

--- postgresql/executer.sh
+++ postgresql/executer.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+
 docker-compose up -d redis postgres
 
 docker-compose up -d superset
 
 docker-compose exec superset superset-demo
 
-docker exec -it postgresql_postgres_1 bash
+#import the db dump exported during postgres run. use environment variables instead of superset.
+
+docker exec -it postgresql_postgres_1  psql -U superset -d superset -f /docker-entrypoint-initdb.d/sql_dump.sql
+
+#login into postgres console -- postgresql_postgres_1 is the name of your postgres container
+#docker exec -it postgresql_postgres_1 bash
+
+#psql -U superset -d superset
+
