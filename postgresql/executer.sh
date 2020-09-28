0fa854948bfab183d41d31b013cdad789136a81d
Jayant Kaushal <awakenrogue@gmail.com>
2020-09-28 23:18:49 +0530

    Updated README.md

--- postgresql/executer.sh
+++ postgresql/executer.sh
@@ -1,16 +1,9 @@
-# harsh_docker_postgres
-A repo for harsh
+#!/bin/bash
 
-# Install the docker compose
 docker-compose up -d redis postgres
 
 docker-compose up -d superset
 
 docker-compose exec superset superset-demo
 
-
-# login into postgres container and look at your postgres db
 docker exec -it postgresql_postgres_1 bash
-
-psql -U superset
-
