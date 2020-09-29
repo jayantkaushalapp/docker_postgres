576a20c8aed984968ac8317eac85afc8b949aff7
Jayant Kaushal <awakenrogue@gmail.com>
2020-09-29 19:31:03 +0530

    adding executer and teardown files.

--- postgresql/executer.sh
+++ postgresql/executer.sh
@@ -5,7 +5,7 @@ docker rm -f $(docker ps -a -q)
 docker volume rm $(docker volume ls -q)
 docker-compose down -v
 docker-compose up -d redis postgres
-docker exec -it postgresql_postgres_1  psql -U superset -d example -f /docker-entrypoint-initdb.d/1-sql_dump.sql
+docker exec -it postgresql_postgres_1  psql -U superset -d superset -f /docker-entrypoint-initdb.d/1-sql_dump.sql
 docker-compose up -d superset
 docker-compose exec superset superset-demo
 
